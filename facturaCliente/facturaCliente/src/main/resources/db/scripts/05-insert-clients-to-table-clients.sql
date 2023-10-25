3656fdfa5cc6b04b09aca077739e6e2d2cb02a6d
unknown <marivafp@gmail.com>
2023-10-25 12:19:35 +0200

    addign clients and invoices

---
+++ facturaCliente/facturaCliente/src/main/resources/db/scripts/05-insert-clients-to-table-clients.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (dni, name, pais, premium, fechaNac)
+VALUES ('12345678A', 'Vanessa', 'España', true, '2023-01-15');
---
+++ facturaCliente/facturaCliente/src/main/resources/db/scripts/06-insert-facturas-to-table-facturas.sql
@@ -0,0 +1,2 @@
+INSERT INTO facturas (codFra, total, mes, anyo, clienteDni)
+VALUES (1, 100.0, 'Enero', 2023, '12345678A');
